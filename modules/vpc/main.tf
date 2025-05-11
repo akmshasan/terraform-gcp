f52d52b7bc01cc431823b9b9221b6d79f5241f49
A K M Shafiul Hasan <akmshasan@gmail.com>
2025-05-12 00:41:18 +0800

    Updated all files for proper version constraints

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.11.4"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.29"
+    }
+  }
+}
+
 resource "google_compute_network" "vpc_network" {
   for_each                = var.vpcs
   name                    = each.value.name
