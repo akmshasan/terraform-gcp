b0ad2c67e55385673af1bd938379011a29761b6e
A K M Shafiul Hasan <akmshasan@gmail.com>
2025-05-12 01:05:54 +0800

    Updated subnet module for adding log_config and private_ip_google_access (#4)
    * Updated all files for proper version constraints
    
    * Added new NAt IP
    
    * Updated subnet module for adding log_config and private_ip_google_access

--- modules/subnets/main.tf
+++ modules/subnets/main.tf
@@ -9,9 +9,15 @@ terraform {
 }
 
 resource "google_compute_subnetwork" "subnets" {
-  for_each      = var.subnets
-  name          = each.value.name
-  ip_cidr_range = each.value.cidr
-  region        = each.value.region
-  network       = each.value.network
+  for_each                 = var.subnets
+  name                     = each.value.name
+  ip_cidr_range            = each.value.cidr
+  region                   = each.value.region
+  network                  = each.value.network
+  private_ip_google_access = true
+  log_config {
+    aggregation_interval = "INTERVAL_5_SEC"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
 }
